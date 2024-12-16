3160323cbf38c93f51fb9551899ccfa1e69c5e3c
Shaiful islam <islambuet@gmail.com>
2024-12-17 05:18:46 +0600

    Update AddEdit.vue

--- src/tasks/research/research_sales_team/AddEdit.vue
+++ src/tasks/research/research_sales_team/AddEdit.vue
@@ -4,6 +4,7 @@
       <router-link :to="taskData.api_url" class="mr-2 mb-2 btn btn-sm bg-gradient-primary" ><i class="feather icon-corner-up-left"></i> {{labels.get('label_back')}}</router-link>
       <template v-if="item.exists">
         <button  type="button" class="mr-2 mb-2 btn btn-sm bg-gradient-primary" @click="save(false)"><i class="feather icon-save"></i> {{labels.get('label_save')}}</button>
+        <button  type="button" class="mr-2 mb-2 btn btn-sm bg-gradient-primary" @click="save(true)"><i class="feather icon-plus-square"></i> {{labels.get('label_save_new')}}</button>
       </template>
     </div>
   </div>
@@ -12,10 +13,84 @@
       <div v-if="item.id>0">{{labels.get('label_edit_task')}}({{item.id}})</div>
       <div v-else>{{labels.get('label_new_task')}}</div>
     </div>
-    <div class="card-body">
+    <div class="card-body" style='overflow-x:auto;min-height:250px;'>
       <form id="formSaveItem">
-        <InputTemplate :inputItems="item.inputFields" />
-        <InputTemplate v-if="item.showInputFields2" :inputItems="item.inputFields2" />
+        <table style="width: 2000px;" class="table table-bordered">
+          <thead class="table-active">
+            <tr>
+              <th style="width: 150px;">{{labels.get('label_crop_name')}}</th>
+              <th style="width: 200px;">{{labels.get('label_crop_type_name')}}</th>
+              <th style="width: 100px;">Total Market Size</th>
+              <th style="width: 600px;">Upazila Market Size</th>
+              <th style="width: 200px;">Sowing Period</th>
+              <th style="width: 600px;">Competitors Variety</th>
+            </tr>
+          </thead>
+          <tbody class="table-striped table-hover">
+            <tr class="row_type" v-for="row in item.rows" :key="'row_'+row.id" :id="'type_'+row.id">
+              <td class="col_crop_name">{{row.crop_name}}</td>
+              <td class="col_crop_type_name">{{row.crop_type_name}}</td>
+              <td class="col_market_size_total"></td>
+              <td class="col_upazilas_market_size">
+                <table class="table table-bordered">
+                  <thead>
+                  <tr>
+                    <th style="width: 100px">Upzila</th>
+                    <th style="width: 100px">Market Size</th>
+                    <th style="width: 200px">Unions</th>
+                    <th style="width: 100px"></th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td colspan="3">
+                      <div class="input-group" >
+                        <select class="form-control sel_upazila">
+                          <option v-for="upazila in item.location_upazilas" :value="upazila.id">
+                            {{upazila.name}}
+                          </option>
+                          <option value="0">Other</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td><button type="button" class="mr-2 mb-2 btn btn-sm bg-gradient-primary btn_add_more_upazila"><i class="bi bi-plus-circle"></i> {{labels.get('action_1')}}</button></td>
+                  </tr>
+                  </tbody>
+                </table>
+              </td>
+              <td>
+                <div v-for="i in 12" class="form-check form-check-inline">
+                  <input class="form-check-input" type="checkbox" :id="'sowing_periods_'+i" :value="i"><label class="form-check-label" :for="'sowing_periods_'+i">{{ labels.get('label_month_short_'+i) }}</label>
+                </div>
+              </td>
+              <td>
+                <table class="table table-bordered">
+                  <thead>
+                  <tr>
+                    <th>Company</th>
+                    <th>Variety</th>
+                    <th>Market Size</th>
+                    <th>Reason for the Sales</th>
+                    <th style="width: 100px"></th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td colspan="4">
+                      <div class="input-group" >
+                        <select class="form-control">
+                          <option value="0">Other</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td><button type="button" class="mr-2 mb-2 btn btn-sm bg-gradient-primary"><i class="bi bi-plus-circle"></i> {{labels.get('action_1')}}</button></td>
+                  </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </form>
     </div>
   </div>
@@ -40,267 +115,76 @@ let taskData = inject('taskData')
 let item=reactive({
   id:0,
   exists:false,
-  showInputFields2:false,
   inputFields:{},
-  inputFields2:{},
-  data:{
-    id:0,
-    name:'',
-    crop_id:'',
-    crop_type2_id:'',
-    part_id:taskData.user_locations.part_id,
-    area_id:taskData.user_locations.area_id,
-    territory_id:taskData.user_locations.territory_id,
-  }
+  data:[],
+  location_upazilas:[],
+  location_unions: {},
+  rows:[]
 })
-const setInputFields=async ()=>{
-  item.inputFields= {};
-  await systemFunctions.delay(1);
-  let inputFields={}
-  let key='save_token';
-  inputFields[key] = {
-    name: key,
-    label: labels.get('label_'+key),
-    type:'hidden',
-    default:new Date().getTime(),
-    mandatory:true
-  };
-  if(item.data.part_id>0){
-    key='part_id';
-    let defaultElem=taskData.location_parts.find((elem)=>{ if(elem.id==item.data.part_id){return elem;}});
-    inputFields[key] = {
-      name: key,
-      label: labels.get('label_'+key),
-      type:'textonly',
-      default:defaultElem?defaultElem.name:labels.get('msg_invalid_data'),
-      mandatory:true
-    };
-    if(item.data.area_id>0){
-      key='area_id';
-      let defaultElem=taskData.location_areas.find((elem)=>{ if(elem.id==item.data.area_id){return elem;}});
-      inputFields[key] = {
-        name: key,
-        label: labels.get('label_'+key),
-        type:'textonly',
-        default:defaultElem?defaultElem.name:labels.get('msg_invalid_data'),
-        mandatory:true
-      };
-      if(item.data.territory_id>0){
-        key='territory_name';
-        let defaultElem=taskData.location_territories.find((elem)=>{ if(elem.id==item.data.territory_id){return elem;}});
-        inputFields[key] = {
-          name: key,
-          label: labels.get('label_'+key),
-          type:'textonly',
-          default:defaultElem?defaultElem.name:labels.get('msg_invalid_data'),
-          mandatory:true
-        };
-        key='territory_id';
-        inputFields[key] = {
-          name: 'item[' +key +']',
-          label: labels.get('label_'+key),
-          type:'hidden',
-          default:item.data.territory_id,
-          mandatory:true
-        };
-
-      }
-      else{
-        key='territory_id';
-        inputFields[key] = {
-          name: 'item[' +key +']',
-          label: labels.get('label_'+key),
-          type:'dropdown',
-          options:taskData.location_territories.filter((elem)=>{ if(elem.area_id==item.data.area_id){elem.value=elem.id.toString();elem.label=elem.name;return true}}),
-          default:item.data[key],
-          mandatory:true
-        };
-      }
-
-    }
-    else{
-      key='area_id';
-      inputFields[key] = {
-        name: key,
-        label: labels.get('label_'+key),
-        type:'dropdown',
-        options:taskData.location_areas.filter((elem)=>{ if(elem.part_id==item.data.part_id){elem.value=elem.id.toString();elem.label=elem.name;return true}}),
-        default:item.data[key],
-        mandatory:true
-      };
-      key='territory_id';
-      inputFields[key] = {
-        name: 'item[' +key +']',
-        label: labels.get('label_'+key),
-        type:'dropdown',
-        options:[],
-        default:item.data[key],
-        mandatory:true
-      };
-    }
-  }
-  else{
-    key='part_id';
-    inputFields[key] = {
-      name: key,
-      label: labels.get('label_'+key),
-      type:'dropdown',
-      options:taskData.location_parts.map((elem)=>{ return {value:elem.id,label:elem.name}}),
-      default:item.data[key],
-      mandatory:true
-    };
-    key='area_id';
-    inputFields[key] = {
-      name: key,
-      label: labels.get('label_'+key),
-      type:'dropdown',
-      options:[],
-      default:item.data[key],
-      mandatory:true
-    };
-    key='territory_id';
-    inputFields[key] = {
-      name: 'item[' +key +']',
-      label: labels.get('label_'+key),
-      type:'dropdown',
-      options:[],
-      default:item.data[key],
-      mandatory:true
-    };
-
-  }
-
-  key='crop_id';
-  inputFields[key] = {
-    name: 'crop_id',
-    label: labels.get('label_'+key),
-    type:'dropdown',
-    options:taskData.crops.map((elem)=>{ return {value:elem.id,label:elem.name}}),
-    default:item.data[key],
-    mandatory:true
-  };
-  key='crop_type2_id';
-  inputFields[key] = {
-    name: 'item[' +key +']',
-    label: labels.get('label_'+key),
-    type:'dropdown',
-    options:[],
-    default:item.data[key],
-    mandatory:true
-  };
-  item.inputFields=inputFields;
-}
-const setInputFields2=async (itemData,competitorVarieties)=>{
-  item.inputFields2= {};
-  await systemFunctions.delay(1);
-  let inputFields2={}
-
-  let key='market_size';
-  inputFields2[key] = {
-    name: 'item[' +key +']',
-    label: labels.get('label_'+key),
-    type:'text',
-    default:itemData?itemData[key]:'',
-    class:'float_positive',
-    mandatory:false
-  };
-  key='sowing_periods';
-  inputFields2[key] = {
-    name: 'item[' +key +']',
-    label: labels.get('label_'+key),
-    type:'checkbox',
-    options:Array.from({length: 12}, (elem,index) => { return {'value':index+1+'','label':labels.get('label_month_short_'+(index+1))}}),
-    default:itemData?itemData[key].split(','):[],
-    mandatory:false,
-    inline:true
-  };
-  key='competitor_varieties_ids';
-  inputFields2[key] = {
-    name: 'item[' +key +']',
-    label: labels.get('label_'+key),
-    type:'checkbox',
-    options:competitorVarieties?competitorVarieties.map((elem)=>{ return {value:elem.id+'',label:elem.name}}):[],
-    default:itemData?itemData[key].split(','):[],
-    mandatory:false,
-    inline:false
-  };
-  key='reason_sales';
-  inputFields2[key] = {
-    name: 'item[' +key +']',
-    label: labels.get('label_'+key),
-    type:'textarea',
-    default:itemData?itemData[key]:'',
-    mandatory:false
-  };
-  item.inputFields2=inputFields2;
-}
 $(document).ready(function()
 {
-  $(document).off("change", "#crop_id");
-  $(document).on("change",'#crop_id',function()
+  $(document).off("click", ".btn_add_more_upazila");
+  $(document).on("click",'.btn_add_more_upazila',function()
   {
-    let crop_id=$(this).val();
-    let key='crop_type2_id';
-    item.inputFields[key].options=taskData.crop_types2.filter((item)=>{ if(item.crop_id==crop_id){item.value=item.id.toString();item.label=item.name;return true}})
-    $('#'+key).val('');
-    getItem();
+    let row_type_id=$(this).closest('.row_type').attr('id');
+    let crop_type_id=row_type_id.substring(5);//after type_
+    let upazila_id=$("#"+row_type_id+" .sel_upazila").val();
+    let upazila_name="Other";
+    let unions=[];
+    if(upazila_id>0){
+      for(let i=0;i<item.location_upazilas.length;i++){
+        if(item.location_upazilas[i].id==upazila_id){
+          upazila_name=item.location_upazilas[i].name;
+          break;
+        }
+      }
+      if(item.location_unions[upazila_id]){
+        unions=item.location_unions[upazila_id];
+      }
+    }
+
+    let html='<tr>';
+    html+=('<td>'+upazila_name+'</td>');
+    html+=('<td><input type="text" class="form-control float_positive input_upazila_market_size" /></td>');
+    html+='<td>';
+    for(let i=0;i<unions.length;i++){
+      let union=unions[i];
+      html+=('<div class="form-check form-check-inline"><input class="form-check-input" type="checkbox" id="'+(row_type_id+"_"+union.id)+'" value="'+union.id+'"><label class="form-check-label" for="'+(row_type_id+"_"+union.id)+'">'+union.name+'</label></div>');
+    }
+    html+='</td>';
+    html+='<td><button type="button" class="mr-2 mb-2 btn btn-sm bg-gradient-danger btn_remove_upazila"><i class="bi bi-dash-circle"></i> Remove </button></td>';
+    html+='</tr>';
+    $(this).closest("tr").before(html);
   })
-  $(document).off("change", "#part_id");
-  $(document).on("change",'#part_id',function()
-  {
-    let part_id=$(this).val();
-    let key='area_id';
-    item.inputFields[key].options=taskData.location_areas.filter((item)=>{ if(item.part_id==part_id){item.value=item.id.toString();item.label=item.name;return true}})
-    $('#'+key).val('');
-    key='territory_id';
-    item.inputFields[key].options=[];
-    $('#'+key).val('');
-    getItem();
-  })
-  $(document).off("change", "#area_id");
-  $(document).on("change",'#area_id',function()
-  {
-    let area_id=$(this).val();
-    let key='territory_id';
-    item.inputFields[key].options=taskData.location_territories.filter((item)=>{ if(item.area_id==area_id){item.value=item.id.toString();item.label=item.name;return true}})
-    $('#'+key).val('');
-    getItem();
-  })
-  $(document).off("change", "#territory_id");
-  $(document).off("change", "#crop_type2_id");
-  $(document).on("change",'#territory_id,#crop_type2_id',function()
-  {
-    getItem();
-  })
+  $(document).off("click", ".btn_remove_upazila");
+  $(document).on("click",'.btn_remove_upazila',function(){
+    let row_type_id=$(this).closest('.row_type').attr('id');
+    $(this).closest('tr').remove();
+    calculateTotalMarketSize(row_type_id);
+  });
+  $(document).off("input", ".input_upazila_market_size");
+  $(document).on("input",'.input_upazila_market_size',function(){
+    let row_type_id=$(this).closest('.row_type').attr('id');
+    calculateTotalMarketSize(row_type_id);
+  });
 });
-const getItem=async ()=>{
-  item.showInputFields2=false;
-  await systemFunctions.delay(1);
-  let territory_id=$('input[name="item[territory_id]"]').val();
-  let crop_type2_id=$('#crop_type2_id').val();
-  if(territory_id>0 && crop_type2_id>0){
-
-    await axios.get(taskData.api_url+'/get-item-by-croptype2_id-territory_id/'+crop_type2_id+'/'+territory_id).then((res)=>{
-      if (res.data.error == "") {
-        setInputFields2(res.data.item,res.data.competitor_varieties);
-        item.showInputFields2=true;
-      }
-      else{
-        toastFunctions.showResponseError(res.data)
-      }
-    });
-  }
-
-
-}
-
 const save=async (save_and_new)=>{
   let formData=new FormData(document.getElementById('formSaveItem'))
   await axios.post(taskData.api_url+'/save-item',formData).then((res)=>{
     if (res.data.error == "") {
       globalVariables.loadListData=true;
       toastFunctions.showSuccessfullySavedMessage();
-      router.push(taskData.api_url)
+      if(save_and_new){
+        if(item.id>0){
+          router.push(taskData.api_url+"/add")
+        }
+        else{
+          setInputFields();
+        }
+      }
+      else{
+        router.push(taskData.api_url)
+      }
     }
     else{
       toastFunctions.showResponseError(res.data)
@@ -308,7 +192,62 @@ const save=async (save_and_new)=>{
   });
 
 }
-      setInputFields();
-      item.exists=true;
+const getItem=async ()=>{
+  await axios.get(taskData.api_url+'/'+taskData.analysis_year_id+'/get-item/'+ item.id).then((res)=>{
+    if (res.data.error == "") {
+      item.data=res.data.data;
+      item.location_upazilas=res.data.location_upazilas;
+      item.location_unions=res.data.location_unions;
+      let rows=[];
+      let prev_crop_name="";
+      for(let i=0;i<taskData.crop_types.length;i++){
+        let crop_type=taskData.crop_types[i];
+        let row={};
+        row['id']=crop_type['id'];
+        if(prev_crop_name!=crop_type['crop_name']){
+          row['crop_name']=crop_type['crop_name'];
+          prev_crop_name=crop_type['crop_name'];
+        }
+        else{
+          row['crop_name']='';
+        }
+        row['crop_type_name']=crop_type['name'];
+        rows.push(row);
+      }
+      item.rows=rows;
 
+      item.exists=true;
+    }
+    else{
+      toastFunctions.showResponseError(res.data)
+    }
+  });
+}
+item.id=route.params['item_id']?route.params['item_id']:0;
+if(item.id>0){
+  if(!(taskData.permissions.action_2)){
+    toastFunctions.showAccessDenyMessage();
+  }
+  else{
+    getItem();
+  }
+}
+else{
+  if(!(taskData.permissions.action_1)){
+    toastFunctions.showAccessDenyMessage();
+  }
+  else{
+    setInputFields();
+    item.exists=true;
+  }
+}
+const calculateTotalMarketSize=(typeId)=>{
+  let totalMarketSize=0;
+  $('#'+typeId+" .input_upazila_market_size").each( function (){
+    totalMarketSize+=(+$(this).val());
+  });
+
+  $('#'+typeId+" .col_market_size_total").text(totalMarketSize);
+
+}
 </script>

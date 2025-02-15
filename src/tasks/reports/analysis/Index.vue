19cbd1313a8d8387f4ff26f311d37770c1753a64
Shaiful Islam <shaiful.islam@aclusterllc.com>
2025-02-15 12:46:15 +0600

    Analysis report filter started

---
+++ src/tasks/reports/analysis/Index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div v-if="taskData.permissions.action_0==1">
+    <Search/>
+    <div v-show="taskData.method=='list'">
+
+    </div>
+
+  </div>
+</template>
+<script setup>
+import globalVariables from "@/assets/globalVariables";
+import systemFunctions from "@/assets/systemFunctions";
+import toastFunctions from "@/assets/toastFunctions";
+import labels from '@/labels'
+import {provide, reactive, watch} from 'vue'
+import {useRoute,useRouter} from 'vue-router';
+import axios from 'axios';
+import Search from './Search.vue'
+
+
+globalVariables.loadListData=true;
+const route =useRoute()
+const router =useRouter()
+
+let taskData=reactive({
+  api_url:systemFunctions.getTaskBaseURL(import.meta.url),
+  method:'',
+  permissions:{},
+  items: {data:[]},   //from Laravel server with pagination and info
+  itemsFiltered: [],    //for display
+  columns:{all:{},hidden:[],sort:{key:'',dir:''}},
+  pagination: {current_page: 1,per_page_options: [10,20,50,100,500,1000],per_page:-1,show_all_items:true},
+  analysis_years:[],
+  location_parts:[],
+  location_areas:[],
+  location_territories:[],
+  location_districts:[],
+  location_upazilas:[],
+  location_unions:[],
+
+  crops:[],
+  crop_types:[],
+  principals :[],
+  competitors:[],
+
+})
+labels.add([{language:globalVariables.language,file:'tasks'+taskData.api_url+'/labels.js'}])
+
+const routing=async ()=>{
+
+}
+watch(route, () => {
+  routing();
+})
+
+const init=async ()=>{
+  await axios.get(taskData.api_url+'/initialize').then((res)=>{
+    if (res.data.error == "") {
+      taskData.permissions=res.data.permissions;
+      taskData.analysis_years=res.data.analysis_years;
+      taskData.location_parts=res.data.location_parts;
+      taskData.location_areas=res.data.location_areas;
+      taskData.location_territories=res.data.location_territories;
+      taskData.location_districts=res.data.location_districts;
+      taskData.location_upazilas=res.data.location_upazilas;
+      taskData.location_unions=res.data.location_unions;
+      taskData.crops=res.data.crops;
+      taskData.crop_types=res.data.crop_types;
+      taskData.principals=res.data.principals;
+      taskData.competitors=res.data.competitors;
+      if(res.data.hidden_columns){
+        taskData.columns.hidden=res.data.hidden_columns;
+      }
+      routing();
+    }
+    else{
+      toastFunctions.showResponseError(res.data)
+    }
+  });
+}
+taskData.setFilteredItems=()=>{
+  taskData.itemsFiltered=systemFunctions.getFilteredItems(taskData.items.data,taskData.columns);
+}
+provide('taskData',taskData)
+if(!(globalVariables.user.id>0)){
+  router.push("/login")
+}
+else{
+  init();
+}
+</script>
---
+++ src/tasks/reports/analysis/labels.js
@@ -0,0 +1,86 @@
+export default {
+    label_task:{
+        en:"Analysis Report",
+        bn:'Variety Report'
+    },
+    label_analysis_year_id :{
+        en:"Analysis Year",
+        bn:'ব্যবহারকারীরা'
+    },
+    label_part_id:{
+        en:"Part",
+        bn:"কর্মচারী আইডি"
+    },
+    label_part_name:{
+        en:"part",
+        bn:"কর্মচারী আইডি"
+    },
+    label_area_id:{
+        en:"Area",
+        bn:"কর্মচারী আইডি"
+    },
+    label_area_name:{
+        en:"Area",
+        bn:"কর্মচারী আইডি"
+    },
+    label_territory_id:{
+        en:"Territory",
+        bn:"কর্মচারী আইডি"
+    },
+    label_territory_name:{
+        en:"Territory",
+        bn:"কর্মচারী আইডি"
+    },
+    label_district_id:{
+        en:"District",
+        bn:"কর্মচারী আইডি"
+    },
+    label_district_name:{
+        en:"District",
+        bn:"কর্মচারী আইডি"
+    },
+    label_upazila_id:{
+        en:"Upazila",
+        bn:"কর্মচারী আইডি"
+    },
+    label_upazila_name:{
+        en:"Upazila",
+        bn:"কর্মচারী আইডি"
+    },
+    label_crop_id:{
+        en:"Crop",
+        bn:"Crop"
+    },
+    label_crop_name:{
+        en:"Crop",
+        bn:"Crop"
+    },
+    label_crop_type_id :{
+        en:"Type",
+        bn:"Type"
+    },
+    label_crop_type_name :{
+        en:"Type",
+        bn:"Type"
+    },
+    label_variety:{
+        en:"Variety",
+        bn:"Variety"
+    },
+    label_principal_id:{
+        en:"Principal",
+        bn:"Principal"
+    },
+    label_principal_name:{
+        en:"Principal",
+        bn:"Principal"
+    },
+    label_competitor_id:{
+        en:"Competitor",
+        bn:"Competitor"
+    },
+    label_competitor_name:{
+        en:"Competitor",
+        bn:"Competitor"
+    },
+}

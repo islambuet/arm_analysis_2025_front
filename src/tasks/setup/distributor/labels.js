c601fc7beee7941e9a77a56a678740edbbab3364
Shaiful Islam <shaiful.islam@aclusterllc.com>
2025-05-02 23:31:59 +0600

    distributor added

---
+++ src/tasks/setup/distributor/labels.js
@@ -0,0 +1,43 @@
+export default {
+    label_task:{
+        en:"Distributors",
+        bn:'ব্যবহারকারীরা'
+    },
+    label_new_task:{
+        en:"New Distributor",
+        bn:'নতুন ব্যবহারকারী'
+    },
+    label_edit_task:{
+        en:"Edit Distributor",
+        bn:'সম্পাদনা ব্যবহারকারী'
+    },
+    label_details_task:{
+        en:"Details of Distributor",
+        bn:'ব্যবহারকারীর বিবরণ'
+    },
+    label_part_id:{
+        en:"Part",
+        bn:"কর্মচারী আইডি"
+    },
+    label_part_name:{
+        en:"Part",
+        bn:"কর্মচারী আইডি"
+    },
+    label_area_id:{
+        en:"Area",
+        bn:"কর্মচারী আইডি"
+    },
+    label_area_name:{
+        en:"Area",
+        bn:"কর্মচারী আইডি"
+    },
+    label_territory_id:{
+        en:"Territory",
+        bn:"কর্মচারী আইডি"
+    },
+    label_territory_name:{
+        en:"Territory",
+        bn:"কর্মচারী আইডি"
+    },
+
+}

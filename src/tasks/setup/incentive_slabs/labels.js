ceec4daafc76a1c999726abf911cd58332a04701
Shaiful Islam <islambuet@gmail.com>
2025-08-12 17:40:03 +0600

    incentive_slabs

---
+++ src/tasks/setup/incentive_slabs/labels.js
@@ -0,0 +1,18 @@
+export default {
+    label_task:{
+        en:"Incentive Slabs",
+        bn:'ব্যবহারকারীরা'
+    },
+    label_new_task:{
+        en:"New Slab",
+        bn:'নতুন ব্যবহারকারী'
+    },
+    label_edit_task:{
+        en:"Edit Slab",
+        bn:'সম্পাদনা ব্যবহারকারী'
+    },
+    label_details_task:{
+        en:"Details Slab",
+        bn:'ব্যবহারকারীর বিবরণ'
+    },
+}

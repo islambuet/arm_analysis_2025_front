b069ee68c3b48b6778209e79926e27f81e61b9be
Shaiful Islam <islambuet@gmail.com>
2025-12-08 14:51:51 +0600

    Seasons

---
+++ src/tasks/setup/seasons/AddEdit.vue
@@ -0,0 +1,213 @@
+<template>
+  <div class="card d-print-none mb-2">
+    <div class="card-body">
+      <router-link :to="taskData.api_url" class="mr-2 mb-2 btn btn-sm bg-gradient-primary" ><i class="feather icon-corner-up-left"></i> {{labels.get('label_back')}}</router-link>
+      <template v-if="item.exists">
+        <button  type="button" class="mr-2 mb-2 btn btn-sm bg-gradient-primary" @click="save(false)"><i class="feather icon-save"></i> {{labels.get('label_save')}}</button>
+        <button  type="button" class="mr-2 mb-2 btn btn-sm bg-gradient-primary" @click="save(true)"><i class="feather icon-plus-square"></i> {{labels.get('label_save_new')}}</button>
+      </template>
+    </div>
+  </div>
+  <div class="card d-print-none mb-2" v-if="item.exists">
+    <div class="card-header">
+      <div v-if="item.id>0">{{labels.get('label_edit_task')}}({{item.id}})</div>
+      <div v-else>{{labels.get('label_new_task')}}</div>
+    </div>
+    <div class="card-body">
+      <form id="formSaveItem">
+        <InputTemplate :inputItems="item.inputFields" />
+      </form>
+    </div>
+  </div>
+</template>
+<script setup>
+
+import globalVariables from "@/assets/globalVariables";
+import systemFunctions from "@/assets/systemFunctions";
+import toastFunctions from "@/assets/toastFunctions";
+import labels from '@/labels'
+
+import {useRouter} from "vue-router";
+import {inject, reactive} from "vue";
+import axios from "axios";
+import InputTemplate from '@/components/InputTemplate.vue';
+import {useRoute} from "vue-router/dist/vue-router";
+
+
+const route =useRoute()
+const router =useRouter()
+let taskData = inject('taskData')
+
+let item=reactive({
+  id:0,
+  exists:false,
+  inputFields:{},
+  data:{
+    id:0,
+    name:'',
+    ordering:99,
+    status:'Active',
+    month_1:0,
+    month_2:0,
+    month_3:0,
+    month_4:0,
+    month_5:0,
+    month_6:0,
+    month_7:0,
+    month_8:0,
+    month_9:0,
+    month_10:0,
+    month_11:0,
+    month_12:0,
+  }
+})
+const setInputFields=async ()=>{
+  item.inputFields= {};
+  await systemFunctions.delay(1);
+  let inputFields={}
+  let key='save_token';
+  inputFields[key] = {
+    name: key,
+    label: labels.get('label_'+key),
+    type:'hidden',
+    default:new Date().getTime(),
+    mandatory:true
+  };
+  key='id';
+  inputFields[key] = {
+    name: key,
+    label: labels.get('label_'+key),
+    type:'hidden',
+    default:item.data[key],
+    mandatory:true
+  };
+  key='name';
+  inputFields[key] = {
+    name: 'item[' +key +']',
+    label: labels.get('label_'+key),
+    type:'text',
+    default:item.data[key],
+    mandatory:true
+  };
+  key='ordering';
+  inputFields[key] = {
+    name: 'item[' +key +']',
+    label: labels.get('label_'+key),
+    type:'number',
+    default:item.data[key],
+    mandatory:false
+  };
+  for(let i=1;i<13;i++){
+    key='month_'+i;
+    inputFields[key] = {
+      name: 'item[' +key +']',
+      label: labels.get('label_month_short_'+i),
+      type:'dropdown',
+      options:[{label:"Yes",value:1},{label:"No",value:0}],
+      default:item.data[key],
+      mandatory:true,
+      noselect:true,
+    };
+  }
+  key='status';
+  inputFields[key] = {
+    name: 'item[' +key +']',
+    label: labels.get('label_'+key),
+    type:'dropdown',
+    options:[{label:"Active",value:'Active'},{label:"In-Active",value:'In-Active'}],
+    default:item.data[key],
+    mandatory:true
+  };
+  item.inputFields=inputFields;
+
+}
+const save=async (save_and_new)=>{
+  let formData=new FormData(document.getElementById('formSaveItem'))
+  await axios.post(taskData.api_url+'/save-item',formData).then((res)=>{
+    if (res.data.error == "") {
+      globalVariables.loadListData=true;
+      toastFunctions.showSuccessfullySavedMessage();
+      if(save_and_new){
+        if(item.id>0){
+          router.push(taskData.api_url+"/add")
+        }
+        else{
+          setInputFields();
+        }
+      }
+      else{
+        router.push(taskData.api_url)
+      }
+    }
+    else{
+      toastFunctions.showResponseError(res.data)
+    }
+  });
+
+}
+const getItem=async ()=>{
+  await axios.get(taskData.api_url+'/get-item/'+ item.id).then((res)=>{
+    if (res.data.error == "") {
+      item.data=res.data.item;
+      setInputFields();
+      item.exists=true;
+    }
+    else{
+      toastFunctions.showResponseError(res.data)
+    }
+  });
+}
+  item.id=route.params['item_id']?route.params['item_id']:0;
+
+$(document).ready(function()
+{
+  $(document).off("change", "#part_id");
+  $(document).on("change",'#part_id',async function()
+  {
+    let part_id=$(this).val();
+    let key='area_id';
+    item.inputFields[key].options=taskData.location_areas.filter((temp)=>{ if(temp.part_id==part_id){temp.value=temp.id.toString();temp.label=temp.name;return true}})
+    await systemFunctions.delay(1);
+    $('#'+key).val('');
+    key='territory_id';
+    item.inputFields[key].options=[];
+    $('#'+key).val('');
+  })
+  $(document).off("change", "#area_id");
+  $(document).on("change",'#area_id',async function()
+  {
+    let area_id=$(this).val();
+    let key='territory_id';
+    item.inputFields[key].options=taskData.location_territories.filter((temp)=>{ if(temp.area_id==area_id){temp.value=temp.id.toString();temp.label=temp.name;return true}})
+    await systemFunctions.delay(1);
+    $('#'+key).val('');
+  })
+  $(document).off("change", "#crop_id");
+  $(document).on("change",'#crop_id',async function()
+  {
+    let crop_id=$(this).val();
+    let key='type_id';
+    item.inputFields[key].options=taskData.crop_types.filter((temp)=>{ if(temp.crop_id==crop_id){temp.value=temp.id.toString();temp.label=temp.name;return true}})
+    await systemFunctions.delay(1);
+    $('#'+key).val('');
+
+  })
+});
+  if(item.id>0){
+    if(!(taskData.permissions.action_2)){
+      toastFunctions.showAccessDenyMessage();
+    }
+    else{
+      getItem();
+    }
+  }
+  else{
+    if(!(taskData.permissions.action_1)){
+      toastFunctions.showAccessDenyMessage();
+    }
+    else{
+      setInputFields();
+      item.exists=true;
+    }
+  }
+</script>
---
+++ src/tasks/setup/seasons/labels.js
@@ -0,0 +1,20 @@
+export default {
+    label_task:{
+        en:"Seasons",
+        bn:'ব্যবহারকারীরা'
+    },
+    label_new_task:{
+        en:"New Season",
+        bn:'নতুন ব্যবহারকারী'
+    },
+    label_edit_task:{
+        en:"Edit Season",
+        bn:'সম্পাদনা ব্যবহারকারী'
+    },
+    label_details_task:{
+        en:"Details Season",
+        bn:'ব্যবহারকারীর বিবরণ'
+    },
+
+
+}

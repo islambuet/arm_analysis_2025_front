5581b603de030f27959d27c41e1e36a206ff5477
Shaiful Islam <islambuet@gmail.com>
2026-01-24 12:17:54 +0600

    variety_sub_types

---
+++ src/tasks/setup/variety_sub_types/labels.js
@@ -0,0 +1,18 @@
+export default {
+    label_task:{
+        en:"Divisions",
+        bn:'ব্যবহারকারীরা'
+    },
+    label_new_task:{
+        en:"New Division",
+        bn:'নতুন ব্যবহারকারী'
+    },
+    label_edit_task:{
+        en:"Edit Division",
+        bn:'সম্পাদনা ব্যবহারকারী'
+    },
+    label_details_task:{
+        en:"Details of Division",
+        bn:'ব্যবহারকারীর বিবরণ'
+    },
+}
